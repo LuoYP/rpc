e107dbab451ff534636ea6e462a8f727db66e0f2
LuoYP <1783508363@qq.com>
2023-04-18 22:12:42 +0800

    update

--- src/main/java/org/example/common/handler/MessageEncoder.java
+++ src/main/java/org/example/common/handler/MessageEncoder.java
@@ -1,13 +1,12 @@
 package org.example.common.handler;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
-import org.example.common.model.RpcMessage_old;
 import org.example.common.utils.SerializeUtil;
 
-@ChannelHandler.Sharable
+@Sharable
 public class MessageEncoder extends MessageToByteEncoder<Object> {
 
     @Override
