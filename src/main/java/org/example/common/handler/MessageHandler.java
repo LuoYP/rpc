2a048f4e87bb5389767576e27bc794f2c5f8f7fe
luoyp <1783508363@qq.com>
2023-04-28 16:06:48 +0800

    代码易读性优化

--- src/main/java/org/example/common/handler/MessageHandler.java
+++ src/main/java/org/example/common/handler/MessageHandler.java
@@ -1,8 +1,8 @@
 package org.example.common.handler;
 
+import cn.hutool.core.util.ClassUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.util.concurrent.Promise;
 import org.example.common.constant.Constants;
 import org.example.common.constant.RpcStatusCode;
 import org.example.common.context.Factory;
@@ -21,52 +21,75 @@ public class MessageHandler extends ChannelInboundHandlerAdapter {
     private static final Logger LOGGER = LoggerFactory.getLogger(MessageHandler.class);
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
         LOGGER.debug("receive a rpc message!");
-        if (msg instanceof RpcRequest) {
-            if (Constants.HEART_BEAT == ((RpcRequest) msg).rpcHeader().messageType()) {
-                //心跳消息不做业务处理，直接返回
-                return;
-            }
-            RpcResponse rpcResponse = new RpcResponse();
-            rpcResponse.setRpcHeader(((RpcRequest) msg).rpcHeader()).setCode(RpcStatusCode.OK);
-
-            RpcLine rpcLine = ((RpcRequest) msg).rpcLine();
-            Class<?> requestClass = this.getClass().getClassLoader().loadClass(rpcLine.className());
-            String requestMethod = rpcLine.methodName();
-            Class<?>[] parameterTypes = rpcLine.parameterTypes();
-            Object[] parameters = ((RpcRequest) msg).rpcContent().content();
-
-            //获取实现类对象
-            if (!Factory.BEAN_WAREHOUSE.containsKey(requestClass)) {
-                rpcResponse.setCode(RpcStatusCode.NOT_FOUND);
-                ctx.channel().writeAndFlush(rpcResponse);
-                return;
-            }
-            Object instance = Factory.getBean(requestClass);
-            Method method;
-            Object result = null;
-            long start = System.currentTimeMillis();
-            try {
-                if (parameterTypes == null || parameterTypes.length == 0) {
-                    method = requestClass.getMethod(requestMethod);
-                    result = method.invoke(instance);
-                } else {
-                    method = requestClass.getDeclaredMethod(requestMethod, parameterTypes);
-                    result = method.invoke(instance, parameters);
-                }
-            } catch (Exception e) {
-                rpcResponse.setCode(RpcStatusCode.SERVER_ERROR);
-            }
-            LOGGER.debug("request {}-{} cost:{}s", requestClass.getName(), requestMethod, (System.currentTimeMillis() - start) / 1000);
-            rpcResponse.setContent(result);
-            ctx.channel().writeAndFlush(rpcResponse);
-        } else if (msg instanceof RpcResponse) {
-            Promise<RpcResponse> rpcResponsePromise = RpcSender.RPC_RESPONSE.get(((RpcResponse) msg).rpcHeader().id());
-            //超时会删除RPC_RESPONSE中对应的key，promise可能不存在
-            if (Objects.nonNull(rpcResponsePromise)) {
-                rpcResponsePromise.setSuccess((RpcResponse) msg);
+        switch (msg) {
+            case RpcRequest request -> processRequest(ctx, request);
+            case RpcResponse response -> processResponse(response);
+            default -> {
             }
         }
+    }
+
+    private void processRequest(ChannelHandlerContext ctx, RpcRequest request) {
+        var messageType = request.rpcHeader().messageType();
+        switch (messageType) {
+            case Constants.HEART_BEAT -> processHeatBeatRequest();
+            case Constants.COMMENT -> processCommonRequest(ctx, request);
+            case Constants.FILE -> processFileRequest(ctx, request);
+            default -> {
+            }
+        }
+    }
+
+    private void processResponse(RpcResponse response) {
+        var rpcResponsePromise = RpcSender.RPC_RESPONSE.get(response.rpcHeader().id());
+        //超时会删除RPC_RESPONSE中对应的key，promise可能不存在
+        if (Objects.nonNull(rpcResponsePromise)) {
+            rpcResponsePromise.setSuccess(response);
+        }
+    }
+
+    private void processHeatBeatRequest() {
+        //do nothing
+    }
+
+    private void processCommonRequest(ChannelHandlerContext ctx, RpcRequest request) {
+        var rpcResponse = new RpcResponse();
+        rpcResponse.setRpcHeader(request.rpcHeader()).setCode(RpcStatusCode.OK);
+
+        RpcLine rpcLine = request.rpcLine();
+        Class<?> requestClass = ClassUtil.loadClass(rpcLine.className());
+        String requestMethod = rpcLine.methodName();
+        Class<?>[] parameterTypes = rpcLine.parameterTypes();
+        Object[] parameters = request.rpcContent().content();
+
+        //获取实现类对象
+        if (!Factory.BEAN_WAREHOUSE.containsKey(requestClass)) {
+            rpcResponse.setCode(RpcStatusCode.NOT_FOUND);
+            ctx.channel().writeAndFlush(rpcResponse);
+            return;
+        }
+        var instance = Factory.getBean(requestClass);
+        Method method;
+        Object result = null;
+        var start = System.currentTimeMillis();
+        try {
+            if (parameterTypes == null || parameterTypes.length == 0) {
+                method = requestClass.getMethod(requestMethod);
+                result = method.invoke(instance);
+            } else {
+                method = requestClass.getDeclaredMethod(requestMethod, parameterTypes);
+                result = method.invoke(instance, parameters);
+            }
+        } catch (Exception e) {
+            rpcResponse.setCode(RpcStatusCode.SERVER_ERROR);
+        }
+        LOGGER.debug("request {}-{} cost:{}s", requestClass.getName(), requestMethod, (System.currentTimeMillis() - start) / 1000);
+        rpcResponse.setContent(result);
+        ctx.channel().writeAndFlush(rpcResponse);
+    }
+
+    private void processFileRequest(ChannelHandlerContext ctx, RpcRequest request) {
     }
 }
