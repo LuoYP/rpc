81f29641b5bc48b28b9389ae6969cc196c7a9763
luoyp <1783508363@qq.com>
2023-05-06 17:20:51 +0800

    支持传输文件

--- src/main/java/org/example/common/constant/RpcContainer.java
+++ src/main/java/org/example/common/constant/RpcContainer.java
@@ -1,7 +1,6 @@
 package org.example.common.constant;
 
 import io.netty.util.concurrent.Promise;
-import org.example.common.io.RpcFile;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
